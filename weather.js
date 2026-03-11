d5b11a9560a063aa7639492d14ca93edf8faed69
Shinemi <simon.lucas.83136@gmail.com>
2026-03-11 09:28:41 +0100

    Add initial HTML structure and weatherInfo class implementation

---
+++ weather.js
@@ -0,0 +1,10 @@
+class weatherInfo {
+    constructor(city, country, temp, humidity, description) {
+        this.city = city;
+        this.country = country;
+        this.temp = temp;
+        this.humidity = humidity;
+        this.description = description;
+    }
+}
+
